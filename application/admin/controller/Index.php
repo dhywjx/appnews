2a2657016f93c2a3c50aa33de9c7f9ec3d3bf1b3
dhywjx <2787811826@qq.com>
2018-03-31 00:23:27 +0800

    入口文件目录改为admin下
    入口文件目录改为admin下

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 王晶旭
+ * Date: 2018/3/31
+ * Time: 0:21
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return "admin/Index/index";
+    }
+}
